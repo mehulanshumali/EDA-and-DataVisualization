d5f0093d2c74a60c629c3e0b0fa7740c5c4b3fa0
mehul-anshumali <mehulanshumali.ma@gmail.com>
2020-07-12 12:57:15 +0530

    Updated

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-mkdir -p ~/.streamlit/echo “\
-[general]\n\
-email = \”mehulanshumali.ma@gmail.com\”\n\
-“ > ~/.streamlit/credentials.tomlecho “\
+mkdir -p ~/.streamlit/
+
+echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
-“ > ~/.streamlit/config.toml
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
